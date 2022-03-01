cf460467c6c23aa439fdf02fd113b2d713af0564
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-02-28 23:22:18 -0300

    2k22

---
+++ index.ts
@@ -0,0 +1,12 @@
+/*
+type Permission = 'Admin' | 'Mod';
+type NoPermission = 'User' | 'Guest';
+
+let Login: Permission;
+  Login = 'Admin';
+
+  Login = 'User';
+*/
+
+
+//Interface serve para documentar ou validar o código, facilitando o desenvolvimento
---
+++ teoria.ts
@@ -0,0 +1,38 @@
+/*
+  Boolean 
+  True or false
+__________________
+  String (Texto)
+  'Gustavo'
+___________________
+  Number (Número)
+  1, 2, 3, 4, 10.99
+___________________
+  Array (Number, String) [Number, String]
+  let pessoas: string[];
+  let idade: number[];
+___________________
+  Object (Qualquer coisa menos boolean, string, number, array)
+  cliente = {
+    id = 10;
+    name = 'Juca';
+  }
+___________________
+  TUPLE
+  let pessoas: [number, string, boolean];
+  pessoas = [1000, 'Gustavo', true];
+___________________
+  ANY
+  let nome: any;
+  nome = [10, 'Gustavo'];
+  nome = true;
+___________________
+  VOID
+  function sendemail(email:string): void{
+
+  }
+  sendemail('gstvoaugusto@gmail.com');
+___________________
+  NULL E UNDEFINED
+  let teste: null;
+  console.log(name) >> undefined; */
